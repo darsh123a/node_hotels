34c0455fa66e21bd8c79aa0de0928d1a052c9d92
adarsh <adarshcsrrsimt@gmail.com>
2024-12-09 10:40:54 +0530

    next version of code

--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -1,15 +1,14 @@
-const express=require("express")
-const menu=require("./../models/menu");
-const router=express.Router()
-router.get("/menu",async(req,res)=>{
-    try{
-      const data=await menu.find()
-      res.send(data)
-    }
-    catch(err){
-      console.log(err)
-    }
-    })
-    
-   
-      module.exports=router;
+const express = require("express");
+const menu = require("./../models/menu");
+const router = express.Router();
+router.get("/menu", async (req, res) => {
+  try {
+    const data = await menu.find();
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+  }
+});
+//("change kr rha hu git ko dekhne ke liye");
+module.exports = router;
+// comment added for check
